06fcc68bac3982e906429d8140cd815c1f08f6ec
Aditya <aditya@Adityas-MBP.domain.name>
2022-12-20 23:19:54 +0530

    recursion

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 8_batch_db
+
+# loop
+recursion
+dynamic programming
+tree
+graph
+matrix
+backtracking
--- recursion.py
+++ recursion.py
@@ -1,5 +1,36 @@
-#this is a file
+
+#      *
+#     ***
+#    *****
+#   *******
+#  *********
+# ***********
 
 
-def abc():
-    print('a')
+# simple start
+def print_star(star,space):
+    print(" "*space,'*'* star)
+    if star > 10:
+        return
+
+    print_star(star+2,space-1)
+    return 
+
+#reverse star
+def print_star(star,space):
+    print(" "*space,'*'* star)
+    if star > 10:
+        return
+
+    print_star(star+2,space-1)
+
+    return 
+
+
+
+print_star(1,9)
+
+
+
+#stack last in first out
+#queue first in first out
